e10fdb6389c8b7d98751bc07db9f0f96f60a4c32
Robert-Roy <robertproy@live.com>
2018-05-21 17:43:07 -0500

    add missing semicolon

--- js/script.js
+++ js/script.js
@@ -266,7 +266,7 @@ function getFoursquareCategories() {
             handleFoursquareResponse(response, thisPlace);
         });
         request.fail(function(){
-            appendFoursquareNoMatchError(thisPlace)
+            appendFoursquareNoMatchError(thisPlace);
         });
     }
 }
